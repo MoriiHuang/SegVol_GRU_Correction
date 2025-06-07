f9c599297efebd8ea7eed932257e605ec06dd6ad
MoriiHuang <miaochaoe9102@163.com>
2025-06-07 16:34:07 +0800

    add Para and FLOP test

--- test_Pama_Flop.py
+++ test_Pama_Flop.py
@@ -14,13 +14,35 @@ model.model.text_encoder.tokenizer = clip_tokenizer
 total_params = sum(p.numel() for p in model.parameters())
 print(f"Number of model parameters: {total_params / 1e6}M")  
 
-from torch.profiler import profile, record_function, ProfilerActivity
+import torch.nn as nn
 
-# 示例输入数据
-input_data = torch.randn(1, 32, 224, 224)  
-# 开始性能分析
-with profile(activities=[ProfilerActivity.FORWARD], record_shapes=True) as prof:
-    model(input_data)
+class WrappedModel(nn.Module):
+    def __init__(self, model):
+        super().__init__()
+        self.model = model
 
-# 打印推理过程中的 FLOPs
-print(f"Number of FLOPs: {prof.total_average().cpu_time / 1e9} GFLOPs") 
+    def forward(self, x):  # x 是输入的 dict（模拟 kwargs）
+        return self.model(**x)
+from fvcore.nn import FlopCountAnalysis, parameter_count
+import torch
+
+# 输入模拟
+input_data = {
+    "image": torch.randn(1, 1, 32, 256, 256),  # 示例：3D图像
+    "zoomed_image": torch.randn(1, 1, 32, 256, 256),
+    "text_prompt": None,
+    "bbox_prompt_group": None,
+    "point_prompt_group": None,
+    "use_zoom": False,
+    "stage":"box",
+    "train_organs": None,
+    "train_labels":  torch.randn(1, 32, 256, 256),
+}
+
+# 包装模型
+wrapped_model = WrappedModel(model.eval()) 
+flops = FlopCountAnalysis(wrapped_model, (input_data,))
+params = parameter_count(wrapped_model)
+
+print(f"FLOPs: {flops.total() / 1e9:.2f} GFLOPs")
+print(f"Params: {params[''] / 1e6:.2f} M")
